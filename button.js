a01d32bd250ec4fa4fac1c1d2d9f6d1e8d62ab64
VivianVriezekolk <vivianvriezekolk@gmail.com>
2017-03-17 22:08:03 +0100

    added button object and done button

--- button.js
+++ button.js
@@ -1,27 +1,25 @@
-var button = function(element, X, Y){
+var button = function(element, X, Y, molecule, currentMolecule){
   this.element = element;
-  var button = game.add.button(X, Y, element, this.actionOnClick, this, 'C','C', 'C');
-  button.scale.setTo(0.08, 0.08);
   console.log(molecule);
-
 
   this.actionOnClick = function(){
     console.log("You clicked on me!!");
-
     if(countNumberOfAtoms(currentMolecule, element) != countNumberOfAtoms(molecule, element)){
   		console.log(countNumberOfAtoms(molecule, element));
   		var atomNew = new atom(element, true);
-  		atomNew.image.inputEnabled = true;
-  		atomNew.image.input.enableDrag();
+  		atomNew.sprite.inputEnabled = true;
+  		atomNew.sprite.input.enableDrag();
   		var scaleX = 0.02;
   		var scaleY = 0.02;
-  		atomNew.image.scale.set(scaleX , scaleY);
-  		atomNew.image.input.enableDrag(true);
-  		atomNew.image.input.enableSnap(32, 32, false, true);
+  		atomNew.sprite.scale.set(scaleX , scaleY);
+  		atomNew.sprite.input.enableDrag(true);
+  		atomNew.sprite.input.enableSnap(32, 32, false, true);
   		currentMolecule.push(atomNew);
   	}
-  }
+  };
 
+  var button = game.add.button(X, Y, element, this.actionOnClick, this);
+  button.scale.setTo(0.02, 0.02);
 
 
 }
