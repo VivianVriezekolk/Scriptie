243ea2493faa891011133c589f193f24c4117993
VivianVriezekolk <vivianvriezekolk@gmail.com>
2017-03-22 12:23:41 +0100

    make pop up

--- atom.js
+++ atom.js
@@ -1,6 +1,26 @@
-atom = function(value, currentAtom) {
+function atom(value, currentAtom, id) {
     this.value = value
+		this.neighbour = [];
+		this.id = id;
 		if(currentAtom){
-			this.image = game.add.sprite(5, 100, value);
-	}
-};
+			this.sprite = game.add.sprite(5, 100, value);
+		}
+
+		// this function adds a connection between atoms.
+		this.addConnection = function(snappedAtom){
+				if(!atomsAreSnapped(this, snappedAtom)){
+					console.log(this.value + " is connected to " + snappedAtom.value);
+					this.neighbour.push(snappedAtom);
+				}
+			}
+
+		// this function removes a connection between atoms.
+		this.removeConnection = function(lostAtom){
+			for(var i=0; i<this.neighbour.length; i++){
+				if(this.neighbour[i].value == lostAtom.value){
+					console.log(lostAtom.value + " is removed from " + this.value);
+					this.neighbour.splice(i, 1);
+				}
+			}
+		}
+}
