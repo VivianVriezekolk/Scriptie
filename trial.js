84fcf10a800b4f346f73e1e50e093d3dc897c9c6
VivianVriezekolk <vivianvriezekolk@gmail.com>
2017-04-09 23:29:34 +0200

    make code for reading in questions

--- trial.js
+++ trial.js
@@ -1,3 +1,26 @@
-function trial(game){
+function Trial(game){
   this.game = game;
+  this.currentQuestionText = [];
+  this.currentMolecule = [];
+  this.molecule = [];
+  this.questions = [];
+
+
+  this.readInQuestions = function(){
+
+  }
+
+  this.NextQuestion = function(){
+
+  }
+
+  this.showQuestion = function(){
+
+  }
+
+
+
+
+
+
 }
