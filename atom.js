cfa373ee74ac6722f9d27314e2151fcd54ba511a
Vivian Vriezekolk <vivianvriezekolk@gmail.com>
2017-05-22 11:40:04 +0200

    Position atoms in grid

--- atom.js
+++ atom.js
@@ -5,8 +5,8 @@ function Atom(value, currentAtom, id, game) {
     this.game = game;
 		if(currentAtom){
       console.log(game.world.centerX + ", " + game.world.centerY);
-      var Xrandom = Math.floor((Math.random() * (640-110)) + 110);
-    	var Yrandom = Math.floor((Math.random() * (455-5)) + 5);
+      var Xrandom = determineXPositionInGrid();
+      var Yrandom = determineYPositionInGrid();
       console.log("X is " + Xrandom + ", Y is " + Yrandom);
 			this.sprite = game.add.sprite(Xrandom, Yrandom, value);
       this.sprite.inputEnabled = true;
@@ -15,6 +15,22 @@ function Atom(value, currentAtom, id, game) {
   		this.sprite.input.enableSnap(32, 32, false, true);
       this.sprite.events.onDragStop.add(deleteAtom, this);
 		}
+
+    function determineXPositionInGrid(){
+      var Xrandom = Math.floor((Math.random() * (640-110)) + 110);
+      while(Xrandom % 32 != 0){
+        Xrandom = Math.floor((Math.random() * (640-110)) + 110);
+      }
+      return Xrandom;
+    }
+
+    function determineYPositionInGrid(){
+      var Yrandom = Math.floor((Math.random() * (455-5)) + 5);
+      while(Yrandom % 32 != 0){
+        Yrandom = Math.floor((Math.random() * (455-5)) + 5);
+      }
+      return Yrandom;
+    }
 
 		// this function adds a connection between atoms.
 		this.addConnection = function(snappedAtom){
