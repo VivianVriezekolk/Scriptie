8c3e5234ad9c93582a2e8444824f39e476464881
Vivian Vriezekolk <vivianvriezekolk@gmail.com>
2017-06-23 23:46:21 +0200

    many improvements

--- public/button.js
+++ public/button.js
@@ -3,13 +3,9 @@ function Button(buttonName, element, X, Y, game){
   this.buttonName = buttonName;
 
   this.actionOnClick = function(){
-    console.log("You clicked on me!!");
-  //  if(countNumberOfAtoms(currentQuestion.currentMolecule, element) != countNumberOfAtoms(currentQuestion.molecule, element)){
-  		console.log(countNumberOfAtoms(game.currentQuestion.molecule, element));
   		var atomNew = new Atom(element, true, game.id, game);
   		game.currentQuestion.currentMolecule.push(atomNew);
 			id = id+1;
-  	//}
   }
   var button = game.add.button(X, Y, buttonName, this.actionOnClick, this);
   button.scale.setTo(0.4, 0.4);
